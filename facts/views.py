8e5a302d6f422b39549a81dc66ea3b2ab7ab9b29
Prof J <benj1@umbc.edu>
2021-03-05 12:19:38 -0500

    Ready for public release!

--- facts/views.py
+++ facts/views.py
@@ -1,7 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from facts.models import CatFact
+
 
 def index(request):
-    return HttpResponse("You look nice today.")
+    facts = CatFact.objects.filter()
+    context = {
+        'facts': facts,
+    }
+    return render(request, 'facts/index.html', context=context)
+
+
+def add(request):
+    fact_text = request.POST['fact']
+    image_url = request.POST['image_url']
+    fact = CatFact(text=fact_text, image_url=image_url)
+    fact.save()
+    return HttpResponseRedirect(reverse('index'))
